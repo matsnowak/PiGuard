594d50c7ed9dbe9d99a426112a5aa76735ee8135
matsnowa <mats.nowak@hotmail.com>
2016-08-21 23:32:12 +0200

    add spring bean

--- server/src/main/java/com/matsnowak/smartalarm/platforms/rpi3mb/RaspberyPi3Platform.java
+++ server/src/main/java/com/matsnowak/smartalarm/platforms/rpi3mb/RaspberyPi3Platform.java
@@ -14,6 +14,7 @@ import com.pi4j.io.gpio.*;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Objects;
@@ -21,6 +22,8 @@ import java.util.Objects;
 /**
  * Created by Mateusz Nowak on 17.08.2016.
  */
+
+@Component // maybe factory
 public class RaspberyPi3Platform implements Platform {
     private final List<GpioPinDigitalInput> inputs = Lists.newLinkedList();
     private SlotAddressMapping<Pin> slotAddressMapping;
